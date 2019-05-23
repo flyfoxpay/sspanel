e7bcd8a0cd601e4a2fc1c05a83e8e85d3477a685
黃柏澄(風雨) <wartw@users.noreply.github.com>
2019-05-23 11:04:15 +0800

    Create config.php

---
+++ config里面档案不可直接上传/config.php
@@ -0,0 +1,6 @@
+<?php
+//修改$System_Config['payment_system']
+$System_Config['payment_system']='flyfoxpay';
+
+//自行新增
+$System_Config['flyfoxpay']=['config'=>['hid'=>'******','key'=>'********','mail'=>'*******@****.***']];				
